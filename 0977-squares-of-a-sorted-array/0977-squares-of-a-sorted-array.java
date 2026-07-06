a5181b30bfc881f58194dcce3ad4a80da8ed7325
Syyed Anas <145304720+sayyed-anas@users.noreply.github.com>
2026-07-06 20:08:55 +0530

    Time: 5 ms (42.2%), Space: 48.1 MB (16.79%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
@@ -25,98 +25,90 @@ class Solution {
         // }
         // return res;
 
-    ArrayList<Integer> neg = new ArrayList<>();
-    ArrayList<Integer> pos = new ArrayList<>();
-    int n = nums.length;
-    int[] res = new int[n];
+    ArrayList<Integer> list1 = new ArrayList<>();
+        ArrayList<Integer> list2 = new ArrayList<>();
+        int n = nums.length;
+        int i = 0;
+        int j = 0;
+        int[] res = new int[n];
+        // System.out.println(n);
 
-    for (int i = 0; i < n; i++) {
-      if (nums[i] < 0) {
-        neg.add(nums[i]);
-      } else {
-        pos.add(nums[i]);
-      }
-    }
+        for (int k = 0; k < n; k++) {
+            if (nums[i] < 0) {
+                list1.add(i, nums[k]);
+                i++;
+            } else {
+                list2.add(j, nums[k]);
+                j++;
+            }
+        }
 
-    if (neg.size() == 0) {
-      for (int i = 0; i < pos.size(); i++) {
-        res[i] = pos.get(i) * pos.get(i);
-      }
-      return res;
-    }
+        if (list1.size() == 0) {
+            for (int k = 0; k < list2.size(); k++) {
+                res[k] = list2.get(k) * list2.get(k);
+            }
+            return res;
+        }
 
-    if (pos.size() == 0) {
-      for (int i = 0; i < neg.size(); i++) {
-        res[i] = neg.get(i) * neg.get(i);
-      }
+        if (list2.size() == 0) {
+            for (int k = 0; k < list1.size(); k++) {
+                res[k] = list1.get(k) * list1.get(k);
+            }
 
-      int i = 0;
-      int j = res.length - 1;
+            i = 0;
+            j = list1.size() - 1;
 
-      while (i < j) {
-        int temp = res[i];
-        res[i] = res[j];
-        res[j] = temp;
+            while (i < j) {
+                int temp = res[j];
+                res[j] = res[i];
+                res[i] = temp;
+                j--;
+                i++;
+            }
 
-        i++;
-        j--;
-      }
+            return res;
+        }
 
-      return res;
-    }
+        for (int l1 = 0; l1 < list1.size(); l1++) {
+            list1.set(l1, list1.get(l1) * list1.get(l1));
+        }
 
-    for (int i = 0; i < pos.size(); i++) {
-      pos.set(i, pos.get(i) * pos.get(i));
-    }
+        i = 0;
+        j = list1.size() - 1;
 
-    for (int i = 0; i < neg.size(); i++) {
-      neg.set(i, neg.get(i) * neg.get(i));
-    }
+        while (i < j) {
+            int temp = list1.get(j);
+            list1.set(j, list1.get(i));
+            list1.set(i, temp);
 
-    int i = 0;
-    int j = neg.size() - 1;
+            i++;
+            j--;
+        }
 
-    while (i < j) {
-      int temp = neg.get(i);
-      neg.set(i, neg.get(j));
-      neg.set(j, temp);
+        for (int s = 0; s < list2.size(); s++) {
+            list2.set(s, list2.get(s) * list2.get(s));
+        }
 
-      i++;
-      j--;
-    }
+        i = 0;
+        j = 0;
+        int id = 0;
 
-    for (int val : neg) {
-      System.out.print(val + " ");
-    }
-    System.out.println();
-    for (int val : pos) {
-      System.out.print(val + " ");
-    }
-    System.out.println();
+        while (i < list1.size() && j < list2.size()) {
+            if (list1.get(i) < list2.get(j)) {
+                res[id++] = list1.get(i++);
+            } else {
+                res[id++] = list2.get(j++);
+            }
+        }
 
-    int left = 0;
-    int right = 0;
-    int id = 0;
+        while (i < list1.size()) {
+            res[id++] = list1.get(i++);
+        }
 
-    while (left < pos.size() && right < neg.size()) {
-      if (pos.get(left) < neg.get(right)) {
-        res[id] = pos.get(left);
-        id++;
-        left++;
-      } else {
-        res[id] = neg.get(right);
-        id++;
-        right++;
-      }
-    }
+        while (j < list2.size()) {
+            res[id++] = list2.get(j++);
+        }
 
-    while (left < pos.size()) {
-      res[id++] = pos.get(left++);
-    }
-    while (right < neg.size()) {
-      res[id++] = neg.get(right++);
-    }
-
-    return res;
+        return res;
     }
 }
