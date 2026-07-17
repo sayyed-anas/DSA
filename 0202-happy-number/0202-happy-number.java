17a30d445a209213c725c82d2a62411f913c1af1
Syyed Anas <145304720+sayyed-anas@users.noreply.github.com>
2026-07-17 07:53:37 +0530

    Time: 1 ms (78.13%), Space: 42.5 MB (51.52%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -17,8 +17,8 @@ class Solution {
         do {
             slow = squaredSum(slow);
             fast = squaredSum(squaredSum(fast));
-        } while(slow != fast && fast != 1);
+        } while(slow != fast);
 
-        return fast == 1;
+        return slow == 1;
     }
 }
