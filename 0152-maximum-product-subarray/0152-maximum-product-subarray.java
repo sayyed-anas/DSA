f589115894682ef25ca6cf142387fcaf659cfd2c
Syyed Anas <145304720+sayyed-anas@users.noreply.github.com>
2026-07-20 08:52:12 +0530

    Time: 2 ms (70.8%), Space: 47 MB (94.69%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -1,13 +1,12 @@
 class Solution {
     public int maxProduct(int[] nums) {
 
+        int n = nums.length;
+        int res = nums[0];
         int minEnding = nums[0];
         int maxEnding = nums[0];
-        int ans = nums[0];
-        int n = nums.length;
 
         for (int i = 1; i < n; i++){
-
             int v1 = nums[i];
             int v2 = minEnding * nums[i];
             int v3 = maxEnding * nums[i];
@@ -15,8 +14,8 @@ class Solution {
             minEnding = Math.min(v1, Math.min(v2,v3));
             maxEnding = Math.max(v1, Math.max(v2,v3));
 
-            ans = Math.max(ans, Math.max(minEnding, maxEnding));
+            res = Math.max(res, Math.max(minEnding, maxEnding));
         }
-        return ans;
+        return res;
     }
 }
