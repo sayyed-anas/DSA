a5ca0a1f8a819cf7b0660b6c07eb7c5c5acef81a
Syyed Anas <145304720+sayyed-anas@users.noreply.github.com>
2026-07-20 10:14:40 +0530

    Time: 7 ms (59.48%), Space: 66.6 MB (93.4%) - LeetHub

--- 1749-maximum-absolute-sum-of-any-subarray/1749-maximum-absolute-sum-of-any-subarray.java
+++ 1749-maximum-absolute-sum-of-any-subarray/1749-maximum-absolute-sum-of-any-subarray.java
@@ -1,12 +1,12 @@
 class Solution {
+    private static int minSum(int[] nums, int n){
 
-    private static int minSum(int[] nums){
         int minEnding = nums[0];
         int res = nums[0];
 
-        for(int i = 1; i < nums.length; i++){
+        for (int i = 1; i < n; i++){
             int v1 = nums[i];
-            int v2 = nums[i] + minEnding;
+            int v2 = minEnding + nums[i];
 
             minEnding = Math.min(v1, v2);
             res = Math.min(res, minEnding);
@@ -14,21 +14,19 @@ class Solution {
         return Math.abs(res);
     }
 
-    private static int maxSum(int[] nums){
-        int maxEnding = nums[0];
+    private static int maxSum(int[] nums, int n){
+
         int res = nums[0];
+        int maxEnding = nums[0];
 
-        for (int i = 1; i < nums.length; i++){
-            int v1 = nums[i];
-            int v2 = nums[i] + maxEnding;
-
-            maxEnding = Math.max(v1, v2);
+        for (int i = 1; i < n; i++){
+            maxEnding = Math.max(nums[i], maxEnding + nums[i]);
             res = Math.max(res, maxEnding);
         }
         return Math.abs(res);
     }
-
     public int maxAbsoluteSum(int[] nums) {
-      return Math.max(minSum(nums), maxSum(nums));
+        int n = nums.length;
+        return Math.max(minSum(nums,n), maxSum(nums,n));
     }
 }
