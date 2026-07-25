3cc38fe079533918b1fda2ccdc5b636dd3512ab2
Syyed Anas <145304720+sayyed-anas@users.noreply.github.com>
2026-07-25 09:16:06 +0530

    Time: 38 ms (21.8%), Space: 59.3 MB (32.7%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -2,43 +2,45 @@ import java.util.*;
 
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
+
         Arrays.sort(nums);
+
         int n = nums.length;
         List<List<Integer>> list = new ArrayList<>();
         
-        for (int i = 0; i < n - 2; i++){
+        for (int i = 0; i < n-2; i++){
 
-            if (i > 0 && nums[i] == nums[i-1]){
+            if (i > 0 && nums[i] == nums[i - 1]){
                 continue;
             }
 
             int left = i + 1;
             int right = n - 1;
-            int target = -1 * nums[i];
+            int currElem = -1 * nums[i];
 
-            while (left < right){
+          while (left < right){
+            int sum = nums[left] + nums[right];
+            if (currElem == sum){
 
-                int sum = nums[left] + nums[right];
+                list.add(new ArrayList<>(Arrays.asList(nums[i],nums[left],nums[right])));
+                left++;
+                right--;
 
-                if (sum == target){
-                    list.add(new ArrayList<>(Arrays.asList(nums[i],nums[left],nums[right])));
+                while (left < n && nums[left] == nums[left - 1]){
                     left++;
-                    right--;
-
-                    while (left < n && nums[left] == nums[left - 1]){
-                        left++;
-                    }
-                    while (right >= 0 && nums[right] == nums[right + 1]){
-                        right--;
-                    }
                 }
-                else if (sum > target){
+                while (right >= 0 && nums[right] == nums[right + 1]){
                     right--;
-                }
-                else {
-                    left++;
                 }
             }
+
+            else if (sum > currElem){
+                right--;
+            }
+            else {
+                left++;
+            }
+        }
         }
         return list;
     }
