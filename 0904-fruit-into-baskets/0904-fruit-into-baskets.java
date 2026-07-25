4e9a6e921729c72177041113a360c8a8b7b4f4d5
Syyed Anas <145304720+sayyed-anas@users.noreply.github.com>
2026-07-25 19:58:09 +0530

    Time: 55 ms (43.19%), Space: 70.6 MB (28.73%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.java
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -1,29 +1,31 @@
 class Solution {
     public int totalFruit(int[] fruits) {
+
         int k = 2;
         int n = fruits.length;
-        int res = -1;
         int low = 0;
-        HashMap<Integer, Integer> hm = new HashMap<>();
+        int max_len = Integer.MIN_VALUE;
+        HashMap<Integer, Integer> freq = new HashMap<>();
 
         for (int high = 0; high < n; high++){
-            hm.put(fruits[high], hm.getOrDefault(fruits[high], 0) + 1);
 
-            while (hm.size() > k){
-                hm.put(fruits[low], hm.get(fruits[low]) - 1);
+            freq.put(fruits[high], freq.getOrDefault(fruits[high], 0) + 1);
 
-                if (hm.get(fruits[low]) == 0){
-                    hm.remove(fruits[low]);
+            while (freq.size() > k){
+                freq.put(fruits[low], freq.get(fruits[low]) - 1);
+
+                if (freq.get(fruits[low]) == 0){
+                    freq.remove(fruits[low]);
                 }
-
                 low++;
             }
 
-            if (hm.size() == k || hm.size() < k){
-                int len = (high - low) + 1;
-                res = Math.max(res, len);
+            if (freq.size() <= k){
+                int len = high - low + 1;
+                max_len = Math.max(max_len, len);
             }
+
         }
-        return res;
+        return max_len == Integer.MIN_VALUE ? 0 : max_len;
     }
 }
