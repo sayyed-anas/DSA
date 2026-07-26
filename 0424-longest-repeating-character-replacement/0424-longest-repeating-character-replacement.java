8c73583fb55425ccd0b204de0d77227a59957027
Syyed Anas <145304720+sayyed-anas@users.noreply.github.com>
2026-07-26 09:19:35 +0530

    Time: 6 ms (98.82%), Space: 46.5 MB (30.84%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -3,23 +3,25 @@ class Solution {
 
         int n = s.length();
         int low = 0;
+        int[] res = new int[26];
         int max_freq = Integer.MIN_VALUE;
-        int[] arr = new int[26];
-        int res = 0;
+        int max_len = 0;
 
         for (int high = 0; high < n; high++){
-            char ch = s.charAt(high);
-            arr[ch - 'A']++;
-            max_freq = Math.max(max_freq,arr[ch - 'A']);
+            
+            char rightChar = s.charAt(high);
+            res[rightChar - 'A']++;
+            max_freq = Math.max(max_freq, res[rightChar - 'A']);
 
-            while ((high - low) + 1 - max_freq > k){
+            while(((high - low) + 1 - max_freq) > k){
+
                 char leftChar = s.charAt(low);
-                arr[leftChar - 'A']--;
+                res[leftChar - 'A']--;
                 low++;
             }
 
-            res = Math.max(res,(high - low) + 1);
+            max_len = Math.max(max_len, (high - low) + 1);
         }
-        return res;
+        return max_len;
     }
 }
