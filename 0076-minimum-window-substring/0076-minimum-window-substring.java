6bc61724991e9a3cf432ef4732e74952f8b37060
Syyed Anas <145304720+sayyed-anas@users.noreply.github.com>
2026-07-26 11:47:33 +0530

    Time: 2 ms (99.77%), Space: 44.8 MB (99.83%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,49 +1,46 @@
 class Solution {
     public String minWindow(String s, String t) {
 
-       if (s.length() < t.length()){
-        return "";
-       }
-
-       int[] freq = new int[128];
-
-       for (char ch : t.toCharArray()){
-        freq[ch]++;
-       }
-
-       int matched = 0;
-       int low = 0;
-       int start = 0;
-       int minLen = Integer.MAX_VALUE;
-
-       for (int high = 0; high < s.length(); high++){
-
-        char rightChar = s.charAt(high);
-
-        if(freq[rightChar] > 0){
-            matched++;
+        if (s.length() < t.length()){
+            return "";
         }
 
-        freq[rightChar]--;
+        int[] freq = new int[128];
+        int low = 0;
+        int matched = 0;
+        int min_len = Integer.MAX_VALUE;
+        int start = 0;
+        int n = s.length();
 
-        while (matched == t.length()){
-
-            if ((high - low) + 1 < minLen){
-                minLen = (high - low) + 1;
-                start = low;
-            }
-
-            char leftChar = s.charAt(low);
-
-            freq[leftChar]++;
-
-            if (freq[leftChar] > 0){
-                matched--;
-            }
-
-            low++;
+        for (char ch : t.toCharArray()){
+            freq[ch]++;
         }
-       }
-       return minLen == Integer.MAX_VALUE ? "" : s.substring(start, start+minLen);
+
+        for (int high = 0; high < n; high++){
+            
+            char rightChar = s.charAt(high);
+
+            if (freq[rightChar] > 0){
+                matched++;
+            }
+            
+            freq[rightChar]--;
+
+            while (matched == t.length()){
+                if ((high - low) + 1 < min_len){
+                    min_len = (high - low) + 1;
+                    start = low;
+                }
+
+                char leftChar = s.charAt(low);
+                freq[leftChar]++;
+
+                if (freq[leftChar] > 0){
+                    matched--;
+                }
+                low++;
+            }
+        }
+        return min_len == Integer.MAX_VALUE ? "" : s.substring(start, start+min_len);
     }
 }
