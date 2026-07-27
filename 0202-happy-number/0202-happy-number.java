fc1d9e128584c6ab398c208e6bb575ee5cfd7f6c
Syyed Anas <145304720+sayyed-anas@users.noreply.github.com>
2026-07-27 10:18:04 +0530

    Time: 1 ms (77.35%), Space: 42.1 MB (95.33%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -1,22 +1,27 @@
 class Solution {
-    private static int mySum(int n){
+
+    private int squaredSum (int n){
+
         int sum = 0;
-        while (n > 0){
+
+        while (n != 0){
             int digit = n % 10;
             sum = sum + digit * digit;
             n = n / 10;
         }
         return sum;
     }
+
     public boolean isHappy(int n){
-       int slow = n;
-       int fast = n;
 
-       do {
-        slow = mySum(slow);
-        fast = mySum(mySum(fast));
-       } while (slow != fast);
+        int slow = n;
+        int fast = n;
 
-       return slow == 1;
+        do {
+            slow = squaredSum(slow);
+            fast = squaredSum(squaredSum(fast));
+        } while (slow != fast);
+
+        return slow == 1;
     }
 }
