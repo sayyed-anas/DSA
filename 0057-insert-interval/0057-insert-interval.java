56d3945504882f8a0aa3240e5247be66a83fcf07
Syyed Anas <145304720+sayyed-anas@users.noreply.github.com>
2026-08-01 08:30:58 +0530

    Time: 2 ms (26.22%), Space: 47 MB (76.24%) - LeetHub

---
+++ 0057-insert-interval/0057-insert-interval.java
@@ -0,0 +1,50 @@
+class Solution {
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+
+        if (intervals.length == 0) {
+            return new int[][] { newInterval };
+        }
+        
+        ArrayList<int[]> list = new ArrayList<>();
+        boolean insert = false;
+
+        for (int i = 0; i < intervals.length; i++){
+            if (insert == false && intervals[i][0] >= newInterval[0]){
+                list.add(new int[]{newInterval[0],newInterval[1]});
+                insert = true;
+            }
+            list.add(new int[]{intervals[i][0], intervals[i][1]});
+        }
+        
+        if (!insert) {
+            list.add(newInterval);
+        }
+
+        int[][] ans = list.toArray(new int[list.size()][]);
+        Arrays.sort(ans, (a,b) -> Integer.compare(a[0],b[0]));
+
+        int start1 = ans[0][0];
+        int end1 = ans[0][1];
+
+        ArrayList<int[]> list1 = new ArrayList<>();
+
+        for (int i = 1; i < ans.length; i++){
+            int start2 = ans[i][0];
+            int end2 = ans[i][1];
+
+            if (end1 >= start2){
+                start1 = start1;
+                end1 = Math.max(end1, end2);
+                continue;
+            }
+
+            list1.add(new int[]{start1,end1});
+
+            start1 = start2;
+            end1 = end2;
+        }
+        list1.add(new int[]{start1,end1});
+
+        return list1.toArray(new int[list1.size()][]);
+    }
+}
