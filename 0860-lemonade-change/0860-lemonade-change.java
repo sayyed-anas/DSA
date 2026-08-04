a905a6d11044ea2425f79d656e1c890ca86baf5e
Syyed Anas <145304720+sayyed-anas@users.noreply.github.com>
2026-08-04 21:01:39 +0530

    Time: 2 ms (97.94%), Space: 72.5 MB (48.85%) - LeetHub

---
+++ 0860-lemonade-change/0860-lemonade-change.java
@@ -0,0 +1,36 @@
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+
+        int five = 0;
+        int ten = 0;
+
+        for (int i = 0; i < bills.length; i++){
+
+            int money = bills[i];
+
+            if (money == 5){
+                five++;
+            }
+            else if (money == 10){
+                if (five == 0){
+                    return false;
+                }
+                five--;
+                ten++;
+            }
+            else if (money == 20){
+                if (ten > 0 && five > 0){
+                    ten--;
+                    five--;
+                }
+                else if (five >= 3){
+                    five = five - 3;
+                }
+                else {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
