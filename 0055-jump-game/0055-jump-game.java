973c7e5558431539a644544893fccaf13a4e0967
Syyed Anas <145304720+sayyed-anas@users.noreply.github.com>
2026-08-07 19:53:06 +0530

    Time: 2 ms (88.76%), Space: 47.9 MB (43.39%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+
+        int n = nums.length;
+        int reach = 0;
+
+        for (int i = 0; i < n; i++){
+            if (reach < i){
+                return false;
+            }
+
+            reach = Math.max(reach, i + nums[i]);
+
+            if (reach >= n - 1){
+                return true;
+            }
+        }
+        return true;                            
+    }
+}
