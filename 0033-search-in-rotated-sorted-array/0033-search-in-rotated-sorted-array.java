dfa989cc5705ab7981e438f588a0333a93d1d334
Syyed Anas <145304720+sayyed-anas@users.noreply.github.com>
2026-08-09 10:07:11 +0530

    Time: 0 ms (100%), Space: 44 MB (28.18%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,57 +1,42 @@
 class Solution {
     public int search(int[] nums, int target) {
-        
-        int n = nums.length;
+
         int low = 0;
-        int high = n - 1;
-        int res = -1;
+        int high = nums.length - 1;
 
-        while (low <= high){
+        while (low <= high) {
 
-            int mid = (low + high) / 2;
+            int mid = low + (high - low) / 2;
 
-            if (nums[mid] > nums[n-1]){
-                low = mid + 1;
+            if (nums[mid] == target) {
+                return mid;
             }
+
+            // Left half is sorted
+            if (nums[low] <= nums[mid]) {
+
+                // Target lies inside left sorted half
+                if (nums[low] <= target && target < nums[mid]) {
+                    high = mid - 1;
+                } 
+                else {
+                    low = mid + 1;
+                }
+            }
+
+            // Right half is sorted
             else {
-                res = mid;
-                high = mid - 1;
+
+                // Target lies inside right sorted half
+                if (nums[mid] < target && target <= nums[high]) {
+                    low = mid + 1;
+                } 
+                else {
+                    high = mid - 1;
+                }
             }
         }
 
-        int low1 = 0;
-        int high1 = res - 1;
-
-        while (low1 <= high1){
-            int mid1 = (low1 + high1) / 2;
-
-            if (nums[mid1] == target){
-                return mid1;
-            }
-            else if (nums[mid1] > target){
-                high1 = mid1 - 1;
-            }
-            else {
-                low1 = mid1 + 1;
-            }
-        }
-
-        int low2 = res;
-        int high2 = n - 1;
-
-        while (low2 <= high2){
-            int mid2 = (low2 + high2) / 2;
-
-            if (nums[mid2] == target){
-                return mid2;
-            }
-            else if (nums[mid2] > target){
-                high2 = mid2 - 1;
-            }
-            else {
-                low2 = mid2 + 1;
-            }
-        }
         return -1;
     }
 }
