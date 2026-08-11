76ab9eed0faefab1558c281ed68b94a8c07d26c3
Syyed Anas <145304720+sayyed-anas@users.noreply.github.com>
2026-08-11 11:38:54 +0530

    Time: 0 ms (100%), Space: 42.1 MB (49.24%) - LeetHub

---
+++ 2119-a-number-after-a-double-reversal/2119-a-number-after-a-double-reversal.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isSameAfterReversals(int num) {
+        
+        int rev1 = 0;
+        int rev2 = 0;
+        int numCheck = num;
+
+        while (num != 0){
+
+            int digit = num % 10;
+            num = num / 10;
+
+            rev1 = rev1 * 10 + digit;
+        }
+
+        while (rev1 != 0){
+
+            int digit = rev1 % 10;
+            rev1 = rev1 / 10;
+
+            rev2 = rev2 * 10 + digit;
+        }
+
+        return rev2 == numCheck;
+    }
+}
