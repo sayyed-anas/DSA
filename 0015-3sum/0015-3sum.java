ae1253a0b49edb66126e9381c0ddf5f9d2751083
Syyed Anas <145304720+sayyed-anas@users.noreply.github.com>
2026-08-29 09:10:21 +0530

    Time: 36 ms (29.47%), Space: 59.5 MB (20.79%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -2,42 +2,42 @@ import java.util.*;
 
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        
+
         Arrays.sort(nums);
         int n = nums.length;
         List<List<Integer>> list = new ArrayList<>();
-        
-        for (int i = 0; i < n-2; i++){
 
-            if (i > 0 && nums[i] == nums[i-1]){
+        for (int i = 0; i < n - 2; i++){
+
+            if (i > 0 && nums[i] == nums[i - 1]){
                 continue;
             }
-            
+
             int target = -1 * nums[i];
-            int low = i + 1;
-            int high = n - 1;
+            int left = i + 1;
+            int right = n - 1;
 
-            while (low < high){
-                
-                int sum = nums[low] + nums[high];
+            while (left < right){
 
-                if (sum == target){
-                    list.add(new ArrayList<>(Arrays.asList(nums[i], nums[low], nums[high])));
-                    low++;
-                    high--;
+                int currSum = nums[left] + nums[right];
 
-                    while (low < n && nums[low] == nums[low-1]){
-                        low++;
+                if (currSum == target){
+                    list.add(new ArrayList<>(Arrays.asList(nums[i],nums[left],nums[right])));
+                    left++;
+                    right--;
+
+                    while (left < n && nums[left] == nums[left - 1]){
+                        left++;
                     }
-                    while (high >= 0 && nums[high] == nums[high+1]){
-                        high--;
+                    while (right >= 0 && nums[right] == nums[right + 1]){
+                        right--;
                     }
                 }
-                else if (sum > target){
-                    high--;
+                else if (currSum < target){
+                    left++;
                 }
                 else {
-                    low++;
+                    right--;
                 }
             }
         }
