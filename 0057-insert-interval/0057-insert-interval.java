c8ca8621af9ceaa7b42725f18af8b9626f9171e9
Syyed Anas <145304720+sayyed-anas@users.noreply.github.com>
2026-09-05 09:59:59 +0530

    Time: 1 ms (98.15%), Space: 46.7 MB (96.9%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -1,50 +1,51 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
 
-        if (intervals.length == 0) {
-            return new int[][] { newInterval };
+        if (intervals.length == 0){
+            return new int[][]{newInterval};
         }
-        
+
         ArrayList<int[]> list = new ArrayList<>();
         boolean insert = false;
 
         for (int i = 0; i < intervals.length; i++){
+
             if (insert == false && intervals[i][0] >= newInterval[0]){
-                list.add(new int[]{newInterval[0],newInterval[1]});
+                list.add(newInterval);
                 insert = true;
             }
-            list.add(new int[]{intervals[i][0], intervals[i][1]});
+            
+            list.add(intervals[i]);
         }
 
-        if (!insert) {
+        if (!insert){
             list.add(newInterval);
         }
 
-        int[][] ans = list.toArray(new int[list.size()][]);
-        
+        int[][] interVals = list.toArray(new int[list.size()][]);
 
-        int start1 = ans[0][0];
-        int end1 = ans[0][1];
+        int firstStart = interVals[0][0];
+        int firstEnd = interVals[0][1];
+        ArrayList<int[]> l = new ArrayList<>();
 
-        ArrayList<int[]> list1 = new ArrayList<>();
+        for (int i = 1; i < interVals.length; i++){
 
-        for (int i = 1; i < ans.length; i++){
-            int start2 = ans[i][0];
-            int end2 = ans[i][1];
+            int secondStart = interVals[i][0];
+            int secondEnd = interVals[i][1];
 
-            if (end1 >= start2){
-                start1 = start1;
-                end1 = Math.max(end1, end2);
+            if (firstEnd >= secondStart){
+                firstStart = firstStart;
+                firstEnd = Math.max(firstEnd, secondEnd);
                 continue;
             }
 
-            list1.add(new int[]{start1,end1});
-
-            start1 = start2;
-            end1 = end2;
+            l.add(new int[]{firstStart, firstEnd});
+            firstStart = secondStart;
+            firstEnd = secondEnd;
         }
-        list1.add(new int[]{start1,end1});
 
-        return list1.toArray(new int[list1.size()][]);
+        l.add(new int[]{firstStart,firstEnd});
+
+        return l.toArray(new int[l.size()][]);
     }
 }
