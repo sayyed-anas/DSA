7cf1a0ae4b49f73760bb480e31c896d873f10d07
Syyed Anas <145304720+sayyed-anas@users.noreply.github.com>
2026-09-13 10:11:01 +0530

    Time: 5 ms (82.7%), Space: 45.9 MB (52.57%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+
+        if (x < 0){
+            return false;
+        }
+        
+        int rev = 0;
+        int n = x;
+
+        while (x != 0){
+
+            int lastDigit = x % 10;
+            x = x / 10;
+
+            rev = (rev * 10) + lastDigit;
+        }
+
+        if (rev == n){
+            return true;
+        }
+
+        return false;
+    }
+}
